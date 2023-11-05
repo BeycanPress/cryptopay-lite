a439bcf8de6e5c293302ab4e54e433f3cd2bdab5
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2023-11-05 18:59:27 +0800

    added hpos support and plugin list links

--- app/Loader.php
+++ app/Loader.php
@@ -16,17 +16,21 @@ class Loader extends PluginHero\Plugin
         
         $this->feedback(true, 'cryptopay-wc-lite');
 
-        add_filter('plugin_row_meta', function(array $links, string $file) {
-            if ($file === plugin_basename($this->pluginFile)) {
-                $links[] = '<b><a href="https://beycanpress.com/product/cryptopay-all-in-one-cryptocurrency-payments-for-wordpress/?utm_source=lite_version&utm_medium=plugins_list" target="_blank">' . __('Buy premium', 'cryptopay_lite') . '</a></b>';
-            }
-            return $links;
-        }, 10, 2);
+        add_filter('plugin_action_links_' . plugin_basename($this->pluginFile), [$this, 'pluginActionLinks']);
 
         add_action('plugins_loaded', function() {
             $this->apiUrl = (new Api())->getUrl();
             new WooCommerce\Register();
         });
+    }
+
+    public function pluginActionLinks(array $links) : array
+    {
+        $links[] = '<a href="https://beycanpress.com/product/cryptopay-all-in-one-cryptocurrency-payments-for-wordpress/?utm_source=lite_version&utm_medium=plugins_list" style="color: #389e38;font-weight: bold;" target="_blank">' . __( 'Buy Premium', 'cryptopay_lite' ) . '</a>';
+        $links[] = '<a href="' . admin_url( 'admin.php?page=cryptopay_lite_settings' ) . '">' . __( 'Settings', 'cryptopay_lite' ) . '</a>';
+        $links[] = '<a href="https://beycanpress.gitbook.io/cryptopay-docs/" target="_blank">' . __( 'Documentation', 'cryptopay_lite' ) . '</a>';
+
+        return $links;
     }
 
     public function adminProcess() : void
--- index.php
+++ index.php
@@ -149,6 +149,12 @@ function cryptoPayLiteGetPHPMajorVersion() {
     return $version[0] . '.' . $version[1];
 }
 
+add_action('before_woocommerce_init', function() {
+    if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+    }
+});
+
 $cryptoPayLiteTestedPHPVersions = array('7.4', '8.1');
 if (!in_array(cryptoPayLiteGetPHPMajorVersion(), $cryptoPayLiteTestedPHPVersions)) {
     add_action('admin_notices', function() use ($cryptoPayLiteTestedPHPVersions) {
