5dfa18ecca1f68110a46e3754edbe65d859dc060
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2023-11-13 15:32:33 +0800

    changed testnet network to sepolia

--- app/EvmBasedChains.php
+++ app/EvmBasedChains.php
@@ -76,12 +76,13 @@ class EvmBasedChains {
         //     ]
         // ],
         "ethereum" => [
-            "id" => 17000,
-            "hexId" => "0x4268",
+            "id" => 11155111,
+            "hexId" => "0xaa36a7",
             "mainnetId" => 1,
-            "name" => "Ethereum Holesky Testnet",
-            "rpcUrl" => "https://ethereum-holesky.publicnode.com/",
-            "explorerUrl" => "https://holesky.etherscan.io/",
+            "name" => "Ethereum Sepolia Testnet (QR)",
+            "rpcUrl" => "https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+            "wsUrl" => "wss://sepolia.infura.io/ws/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+            "explorerUrl" => "https://sepolia.etherscan.io/",
             "nativeCurrency" => [
                 "symbol" => "ETH",
                 "decimals" => 18
