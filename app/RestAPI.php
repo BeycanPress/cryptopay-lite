7ccd8a4f12cb1aa528402ae344af0f4d6dfe1036
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-04-22 12:08:56 +0800

    fixed undefined index problem

--- app/RestAPI.php
+++ app/RestAPI.php
@@ -106,7 +106,7 @@ class RestAPI extends BaseAPI
     public function middleware(mixed $result, \WP_REST_Server $server, \WP_REST_Request $request): mixed
     {
         // check if request is cryptopay
-        if ('cryptopay-lite' === Helpers::getRoutePaths($request->get_route())[0]) {
+        if ('cryptopay-lite' === (Helpers::getRoutePaths($request->get_route())[0] ?? null)) {
             if ($addon = $this->request->getParam('cp_addon')) {
                 try {
                     // check addon
