7ea7efaa707a3b11b8ecdfd0944c46f8b618ec83
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-07-19 18:29:30 +0800

    updated

--- views/pages/home-page/index.php
+++ views/pages/home-page/index.php
@@ -26,69 +26,20 @@
                 <div class="activity-block" style="padding: 20px; box-sizing: border-box; margin:0">
                     <h3>CryptoPay:</h3>
                     <ul>
-                        <li>
-                            <strong>
-                                ✅ All payments reach you directly. So the whole system works as P2P (Peer to Peer).
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ✅ There are no additional transaction fees other than the transaction fee that the paying user pays to the Blockchain network.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ✅ Since all transactions are P2P (Peer to Peer), payment confirmation times are minimal.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ✅ Easily integrable to other plugins with the APIs in the plugin.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ✅ For example, providing services with many extra plugins such as the Donation plugin.
-                            </strong>
-                        </li>
+                        <li><strong>✅ All payments reach you directly. So the whole system works as P2P (Peer to Peer).</strong></li>
+                        <li><strong>✅ No commission is charged to sellers in any way. Only payers pay blockchain transaction fees.</strong></li>
+                        <li><strong>✅ Since all transactions are P2P (Peer to Peer), payment confirmation times are minimal.</strong></li>
+                        <li><strong>✅ Easily integrable to other plugins with the APIs in the plugin (integrated 16+ plugins).</strong></li>
+                        <li><strong>✅ For example, providing services with many extra plugins such as the Donation plugin.</strong></li>
                     </ul>
                     <h3>Other services:</h3>
                     <ul>
-                        <li>
-                            <strong>
-                                ❌ Payments are generally not P2P (Peer to Peer). They first take the payment into their own wallet and then transfer it to you.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ❌ For taking the payment to them first and then transferring it to you.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ❌ The paying user pays transaction fee for Blockchain.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ❌ An additional transaction fee is charged to transfer the payment from their wallet to you, and this is deducted from your earnings.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ❌ The commission amounts that the service generates its own income will be deducted from your earnings.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ❌ Even the fastest networks have very high payment confirmation times, as multiple transfers are made to earn commissions.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                ❌ Generally you can only use it for WooCommerce. To integrate with other plugins, you need a serious programming knowledge.
-                            </strong>
-                        </li>
+                        <li><strong>❌ They transfer the payment first to their wallet to get a commission and then to your wallet.</strong></li>
+                        <li><strong>❌ Payments are usually not P2P (Peer to Peer). Therefore, payment approval times are very long.</strong></li>
+                        <li><strong>❌ The commission amounts that the service generates its own income will be deducted from your earnings.</strong></li>
+                        <li><strong>❌ Even the fastest networks have very high payment confirmation times, as multiple transfers are made to earn commissions.</strong></li>
+                        <li><strong>❌ Generally you can only use it for WooCommerce. To integrate with other plugins, you need a serious programming knowledge.</strong></li>
+                        <li><strong>❌ An additional transaction fee is charged to transfer the payment from their wallet to you, and this is deducted from your earnings.</strong></li>
                     </ul>
                 </div>
             </div>
@@ -101,86 +52,37 @@
                 
                 <div class="activity-block" style="padding: 20px; box-sizing: border-box; margin:0">
                     <ul>
-                        <li>
-                            <strong>
-                                By default, EVM-based networks are supported. To receive payments from other networks such as Bitcoin, Solana and Tron, you need to purchase extra add-ons. You can accept payments from many Web3 wallets integrated for these networks. It is also available on networks that support address transfer (QR Code) payments.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                You can get pay with native money of blockchain networks or with tokens under these networks.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                You can receive payments in your own special currencies (tokens). It is enough to have a price list in one of the APIs you will see in the next article. For example Coin Market Cap and Coin Gecko
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                The Crypto Compare API is currently provided. In addition, there are Coin Market Cap, Coin Gecko, Moralis and Pancake Swap API add-ons. Pancake Swap only supports BSC network while Moralis supports EVM based networks and Solana. Crypto Compare, Coin Market Cap and Coin Gecko support all currencies listed in it, regardless of network.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                If your token has no value in any of the APIs in the previous article, you can set a price for your token against other currencies with the "Custom token prices" field.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                You can easily activate or deactivate the wallets, networks and currencies you want to accept payments from.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                The accuracy of the payments is checked via the RPC API. It even confirms the payment transaction after checking the address received and the amount sent.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                Payment approvals are double-checked both on the front-end and the back-end. That is, even if the user turns off the computer during payment and leaves. If the payment transaction is completely correct, it will be updated as pay completed.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                You can follow all transactions on the Transaction pages. Transaction ID and link, which network the transaction is on. You can see the data for which order was paid, the crypto amount paid, etc.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                You can easily activate and deactivate the networks and currencies you want to receive payments.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                If you are selling products without member registration, you can receive payment from your customers without having to register with the “Only logged in users can pay” feature.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                You can choose what to update the product status to once the payment is complete.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                It can be easily integrated with other plugins with its APIs.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                Dark theme mode is available for your dark themes.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                With Loco Translate, you can easily translate into any language you want.
-                            </strong>
-                        </li>
-                        <li>
-                            <strong>
-                                You can easily move your settings with the backup system.
-                            </strong>
-                        </li>
+                    <li><strong>By default, EVM-based networks are supported. To receive payments from other networks such as Bitcoin, Solana and Tron, you need to purchase extra add-ons. You can accept payments from many Web3 wallets integrated for these networks. It is also available on networks that support address transfer (QR Code) payments.</strong></li>
+
+                    <li><strong>You can get pay with native money of blockchain networks or with tokens under these networks.</strong></li>
+
+                    <li><strong>You can receive payments in your own special currencies (tokens). It is enough to have a price list in one of the APIs you will see in the next article. For example Coin Market Cap and Coin Gecko.</strong></li>
+
+                    <li><strong>The Crypto Compare API is currently provided. In addition, there are Coin Market Cap, Coin Gecko, Moralis and Pancake Swap API add-ons. Pancake Swap only supports BSC network while Moralis supports EVM based networks and Solana. Crypto Compare, Coin Market Cap and Coin Gecko support all currencies listed in it, regardless of network.</strong></li>
+
+                    <li><strong>If your token has no value in any of the APIs in the previous article, you can set a price for your token against other currencies with the "Custom token prices" field.</strong></li>
+
+                    <li><strong>You can easily activate or deactivate the wallets, networks and currencies you want to accept payments from.</strong></li>
+
+                    <li><strong>The accuracy of the payments is checked via the RPC API. It even confirms the payment transaction after checking the address received and the amount sent.</strong></li>
+
+                    <li><strong>Payment approvals are double-checked both on the front-end and the back-end. That is, even if the user turns off the computer during payment and leaves. If the payment transaction is completely correct, it will be updated as pay completed.</strong></li>
+
+                    <li><strong>You can follow all transactions on the Transaction pages. Transaction ID and link, which network the transaction is on. You can see the data for which order was paid, the crypto amount paid, etc.</strong></li>
+
+                    <li><strong>You can easily activate and deactivate the networks and currencies you want to receive payments.</strong></li>
+
+                    <li><strong>If you are selling products without member registration, you can receive payment from your customers without having to register with the “Only logged in users can pay” feature.</strong></li>
+
+                    <li><strong>You can choose what to update the product status to once the payment is complete.</strong></li>
+
+                    <li><strong>It can be easily integrated with other plugins with its APIs.</strong></li>
+
+                    <li><strong>Dark theme mode is available for your dark themes.</strong></li>
+
+                    <li><strong>With Loco Translate, you can easily translate into any language you want.</strong></li>
+
+                    <li><strong>You can easily move your settings with the backup system.</strong></li>
                     </ul>
                 </div>
                 
